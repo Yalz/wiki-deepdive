fc88608922a862e09f0807ad4ad0ce118961f0ac
Jan Robert <jan.robert@cegeka.com>
2022-11-02 16:44:26 +0100

    feat: neo4j integration + cleanup project

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # wiki-deepdive
 
 An attempt to create a tree of connected links between Wikipedia pages to get to know Python
+
+## To run
+
+docker compose up
+
+pip install requirements.txt
+py ./wiki-deepdive.py
--- wiki-deepdive.py
+++ wiki-deepdive.py
@@ -1,103 +1,83 @@
-import urllib
-from subprocess import check_output
-
-import requests
-import yaml
-from anytree import Node, RenderTree
-from anytree.exporter import DictExporter, DotExporter
 from bs4 import BeautifulSoup
+import requests
+import re
+from neo4j import GraphDatabase
 
-wikiUrl = 'https://en.wikipedia.org'
-query = '/wiki/Adelina_(given_name)'
-known_links = []
+class WikiDeepDive:
+    def __init__(self, uri, user, password):
+        self.databaseConnection = GraphDatabase.driver(uri, auth=(user, password))
 
+    def close(self):
+        self.databaseConnection.close()
 
-def get_topic(link):
-    data = urllib.parse.unquote(link.replace('/wiki/', ''))
-    return data.encode().decode()
+    def add_node(self, topic, link):
+        with self.databaseConnection.session() as session:
+            query = (
+                "MERGE (parent:WikiPage { topic: $topic }) "
+                "MERGE (child:WikiPage { topic: $link }) "
 
+                "MERGE (parent)-[r:LINK]->(child) "
+                "RETURN parent, child, r"
+            )
 
-def process_content(html):
-    # parse html content
-    soup = BeautifulSoup(html, "html.parser")
+    def node_exists(self, topic):
+        with self.databaseConnection.session() as session:
+            query = (
+                "OPTIONAL MATCH (n:WikiPage {topic: $topic})"
+                "RETURN n IS NULL AS Predicate"
+            )
+            return (session.run(query, topic=topic).single()[0])
 
-    div = soup.find(id="mw-content-text")
-    div = next(div.children, None)
+    def process_content(self, html):
+        # parse html content
+        soup = BeautifulSoup(html, "html.parser")
 
-    output = BeautifulSoup("", "html.parser")
-    for x in between(div, div.find('p'), div.find('h2')):
-        output.append(x)
-
-    return output
-
-
-def between(html, start, end):
-    found_first_element = False
-
-    for element in html.findAll(['p', 'h2']):
-        if element == end:
-            break
-        if element == start:
-            found_first_element = True
-        if found_first_element:
-            yield element
-
-
-def get_wiki_page(keyword, parent_branch):
-    url = wikiUrl + keyword
-    reqs = requests.get(url)
-
-    soup = process_content(reqs.text)
-
-    children = []
-    for linkTag in soup.find_all('a'):
-        link = linkTag.get('href')
-        if link is not None and link.startswith('/wiki') and \
-                'File:' not in link and \
-                'Category:' not in link and \
-                'Help:' not in link and \
-                'Wikipedia:' not in link and \
-                'Special:' not in link and \
-                'Portal:' not in link and \
-                'User:' not in link and \
-                'Template:' not in link and \
-                'Talk:' not in link and \
-                'Main_Page' not in link and \
-                'Wikipedia_talk:' not in link and \
-                'MOS:' not in link:
-            if link not in known_links:
-                known_links.append(link)
-                children.append(Node(get_topic(link), parent=parent_branch, link=link))
-    print('found ' + str(len(children)) + ' results for keyword : ' + get_topic(keyword))
+        div = soup.find(id="mw-content-text")
 
 
-def get_wiki_tree(query, branch):
-    # L1
-    get_wiki_page(query, branch)
-
-    print('--- parent done ---')
-
-    for child in branch.children:
-        get_wiki_page(child.link, child)
-
-        print('done with child: ' + child.name)
+        output = []
 
 
-def get_export_filename(query, type):
-    return get_topic(query) + '.' + type
+        for x in self.getLinksUntilTag(div, div.find('h2'), "^/wiki/(?!.*:)(?!.*#).*"):
+            output.append(x['href'])
 
+        return output
 
-startBranch = Node(get_topic(query))
+    def getLinksUntilTag(self, html, end, regex):
+        for element in html.findAll(['a','h2']):
+            if element == end:
+                break
+            if element.name == 'a':
+                if element.get('href'): 
+                    if re.match(regex, element['href']):
+                        yield element
 
-get_wiki_tree(query, startBranch)
+    def processPage(self, query, limit):
+        root = wikiUrl + query
+        
+        if (self.node_exists(root)):
+            print("Processing " + query + " with limit " + str(limit))
+            url = wikiUrl + query
+            reqs = requests.get(url)
+            html = reqs.text
 
-for pre, fill, node in RenderTree(startBranch):
-    print("%s%s" % (pre, node.name))
+            links = self.process_content(html)
 
-with open(get_export_filename(query, 'yaml'), "w+") as file:  # doctest: +SKIP
-    yaml.dump(DictExporter().export(startBranch), file)
+            for link in links :
+                linkRef = wikiUrl + link
 
-DotExporter(startBranch).to_dotfile(get_export_filename(query, 'dot'))
+                self.add_node(root, linkRef)
 
-check_output("dot " + get_export_filename(query, 'dot') + " -T svg -o " + get_export_filename(query, 'svg'),
-             shell=True).decode()
+                if ((limit - 1) != 0):
+                    self.processPage(link, limit - 1)
+
+if __name__ == "__main__":
+    parser = WikiDeepDive("bolt://localhost:7687", "neo4j", "test")
+    #parser.print_greeting("hello, world")
+    
+
+    wikiUrl = 'https://en.wikipedia.org'
+    query = '/wiki/Data_storage'
+
+    parser.processPage(query, 0)
+    parser.close()
